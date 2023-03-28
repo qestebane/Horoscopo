cbc1ee18b830aa2a27802e7de8bf79066b04fa14
Elizabeth <elisabelaq@gmail.com>
2023-03-28 16:29:06 -0500

    add dagger hilt

---
+++ app/src/main/java/com/elizabeth/horoscopo/ui/information/InformationViewModel.kt
@@ -0,0 +1,9 @@
+package com.elizabeth.horoscopo.ui.information
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InformationViewModel @Inject constructor() : ViewModel() {
+}
